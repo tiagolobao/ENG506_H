59c20c49829b72793d348dda0e7d7e865191f542
Tiago Britto Lobão <tiago.blobao@gmail.com>
2018-06-22 17:17:58 -0300

    Commit Inicial

---
+++ minimos_quadrados.m
@@ -0,0 +1,49 @@
+% FUNÇÃO  minimos_quadrados
+% @parametro xlin => valores do eixo x
+% @parametro ylin => valores do eixo y
+
+% @parametro fminsearchChoose => escolhe o modo de achar os valores km e k
+%  fminsearchChoose = true => utiliza a função fminsearch do matlab
+%  ''               = false => utiliza o método dos minimos quadrados
+
+
+function [ km,k ] = minimos_quadrados( xlin,ylin, fminsearchChoose )
+
+    nx=length(xlin);
+    ny=length(ylin);
+
+    %verifica se os vetores x e y tem o mesmo no. De elementos
+    if(nx~=ny)
+       error('os vetores x e y não possuem o mesmo tamanho');
+       exit();
+    end
+
+    if (fminsearchChoose)
+        %função a ser minimizada (a de resíduo)
+        funObj = @(c)sum((ylin - c(2) - c(1)*xlin).^2);
+        %configurações da função
+        options = optimset('MaxIter',5000,     ... %Número max de iterações
+                           'MaxFunEvals',5000, ... %Maximum number of function evaluations allowed, a positive integer
+                           'TolFun',1e-6,      ... %Valor de tolerancia Y
+                           'TolX',1e-6 );      ... %Valor de tolerancia X
+        c=[0 0]; %Chute inicial
+        [opt,fval,exitflag]=fminsearch(funObj,c,options);
+
+        km = 1/opt(2);
+        k = opt(1)/opt(2);
+
+    else
+        Sx = sum(xlin);
+        Sy = sum(ylin);
+
+        Sxy=sum(xlin.*ylin);
+        Sxx=sum(xlin.^2);
+
+        a1=(nx*Sxy-Sx*Sy)/(nx*Sxx-Sx.^2); %slope
+        a0=(Sxx*Sy-Sxy*Sx)/((nx*Sxx)-(Sx.^2)); %intercept
+
+        km=1/a0;
+        k=a1/a0;
+    end
+
+end
---
+++ principal.m
@@ -0,0 +1,36 @@
+%% SETUP
+clear;% clc;
+%% INPUT
+x=[ .01 , .05 , .1 , .5 , 1 , 5 , 10 , 50 , 100 ];
+y=[ 6.078e-11, ...
+    7.595e-9, ...
+    6.063e-8, ...
+    5.788e-6, ...
+    1.737e-5, ...
+    2.423e-5, ...
+    2.430e-5, ...
+    2.431e-5, ...
+    2.431e-5  ];
+
+%% LINEARIZAÇÃO DA FUNÇÃO
+xlin = 1./x.^3;
+ylin = 1./y;
+
+%% ---------------------- LETRA (A) ----------------------------- %%
+%% CHAMADA DE FUNÇÃO
+[km,k] = minimos_quadrados(xlin,ylin,true);
+Ymod=(km.*(x.^3))./(k+(x.^3));
+
+
+%% PLOTANDO OS VALORES
+plot(x,y,'markersize',5);
+hold on; grid on;
+plot(x,Ymod,'k');
+xlabel('S (M)'); ylabel('V0 (M/s)');
+title(['a0 = ' num2str(1/km) ';a1 = ' num2str(k*km)]);
+
+
+%% CALCULO DE R2
+sr=sum((y-Ymod).^2);
+r2=1-sr/sum((y-mean(y)).^2);
+fprintf('O valor de R^2 é: %d \n', r2);
